cf016baa5400c6819d36482dbfd4d90509ec153b
R. Jordan Crouser <jcrouser@smith.edu>
2023-03-02 00:08:03 -0500

    Create FuelType.java

---
+++ FuelType.java
@@ -0,0 +1,5 @@
+// In Java, we can use the keyword "enum" to establish simple type that must take as its value
+// one of a set of predefined constant values.
+public enum FuelType {
+    STEAM, INTERNAL_COMBUSTION, ELECTRIC, OTHER;
+}
