44d282a75f67b99efc8cbac2cfee43417ad8dd18
Zoe Neil <zneil@smith.edu>
2024-02-18 20:58:53 -0500

    car (without accessors) complete

--- Car.java
+++ Car.java
@@ -3,29 +3,59 @@ import java.util.ArrayList;
 public class Car {
 
     //attributes
-    ArrayList<String> arrayList;
+    ArrayList<Passenger> passengerList;
     int maxCap;
 
     //constructor
     public Car(int maxCap){
         this.maxCap = maxCap;
-        this.arrayList = new ArrayList<String>(maxCap);
+        this.passengerList = new ArrayList<Passenger>(maxCap);
 
     }
 
     //methods
-
+    public boolean addPassenger(Passenger p){
+        if (this.passengerList.size() < maxCap){
+            this.passengerList.add(p);
+            return true;
+        } else {
+            return false;
+        }
+    }
     
+    public boolean removePassenger(Passenger p){
+        if(this.passengerList.contains(p)){
+            this.passengerList.remove(p);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void printManifest(){
+        if (this.passengerList.size() > 0){
+            System.out.println(this.passengerList);
+            // for (i=0, this.passengerList < i, i++){
+            //     System.out.println(this.passengerList);
+        } else {
+            System.out.println("This car is EMPTY.");
+        }
+    }
 
     //accessors & manipulators
+    public int getCapacity(){
+        return this.maxCap;
+    }
 
+    public int seatsRemaining(){
+        return this.maxCap - this.passengerList.size();
+    }
 
     public static void main(String[] args) {
         Car mycar = new Car(5);
-        System.out.println(mycar.arrayList);
+        System.out.println(mycar.passengerList);
     }
 }
 
-// - accessor-like methods `public int getCapacity()` and `public int seatsRemaining()` that return the maximum capacity and remaining seats, respectively
-// - `addPassenger(Passenger p)` and `removePassenger(Passenger p)` methods to add or remove a `Passenger` from the `Car` and return `True` if the operation was successful, and `False` otherwise. (_Hint: don't forget to check that there are seats available if someone wants to board, and to confirm that the `Passenger` is actually onboard before trying to remove them! If you encounter a problem, you should `return False`._)
-// - and a final method `printManifest()` that prints out a list of all `Passenger`s aboard the car (or "This car is EMPTY." if there is no one on board).
+
+//what attributes need to be private or public
