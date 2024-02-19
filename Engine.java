90d430405a48ca0274f7befb318dc7038764fbc8
Zoe Neil <zneil@smith.edu>
2024-02-18 19:35:50 -0500

    engine without accessors complete

--- Engine.java
+++ Engine.java
@@ -1,3 +1,53 @@
+import javax.net.ssl.TrustManager;
+import javax.swing.plaf.TreeUI;
+
 public class Engine {
 
+    //attributes
+    FuelType fuelType;
+    double currentFuel;
+    double maxFuel;
+
+    //constructor
+    public Engine(FuelType fuelType, double maxFuel){
+        this.fuelType = fuelType;
+        this.maxFuel = maxFuel;
+        this.currentFuel = maxFuel;
+
+    }
+
+    //methods
+    public void refuel(){
+        //reset current fuel level to maximum
+        this.currentFuel = this.maxFuel;
+    }
+
+    public boolean go(){
+        //decrease current fuel level
+        this.currentFuel = this.currentFuel - 5;
+        //print some useful information (remaining fuel level)
+        System.out.println("Remaning Fuel Level:" + this.currentFuel);
+        //return true if fuel level is above 0 and false otherwise
+        if (this.currentFuel>0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //accessors and manipulators
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+    }
 }
+
+
+
+// - appropriate accessors for fuel type, current fuel, max fuel
+
+// Remember, OOP is all about deciding which classes are responsible for which parts of the end solution. As you program, consider which of these attributes/methods should be `public`, and which should be `private`. These questions may be helpful to ask yourself:
---
+++ brainstorming.md
@@ -0,0 +1,8 @@
+ - The `Train` class will have a relationship with the `Engine` class, because the `Engine` is responsible for powering the `Train`.
+ - The `Train` class also has a relationship with the `Car` class: the `Train` has a collection of `Cars` associated with it at any given time, and you can add / remove `Cars` as necessary (without destroying either the `Train` or the `Cars` themselves).
+ - The `Passenger` class has relationships with both the `Car` and `Train` classes (`Passenger`s board `Car`s as their means of using the `Train` to move around more efficiently).
+
+  - Does **another class** need to be able to **read** this value? (If so, it could either be marked `public` or have an `accessor`)
+ - Does **another class** need to be able to **modify** this value? (If so, it could either be marked `public` or have a `manipulator`)
+
+ Train 
